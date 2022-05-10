d3e9f8970c7f2f34ac7184ff3f6beea710780f26
MoMo <melewskip@gmail.com>
2022-05-10 22:12:04 +0200

    18. Publisher Relationships

--- src/main/java/melp/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/melp/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package melp.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -15,6 +14,9 @@ public class Publisher {
     private String city;
     private String state;
     private String zip;
+
+    @OneToMany
+    private Set<Book> books = new HashSet<>();
 
     public Publisher() {
     }
@@ -65,6 +67,14 @@ public class Publisher {
 
     public void setZip(String zip) {
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
