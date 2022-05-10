da792ffa0c59d352260dc3915cfe590b4f26c071
MoMo <melewskip@gmail.com>
2022-05-10 21:29:32 +0200

    16. Spring data repositories

---
+++ src/main/java/melp/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package melp.springframework.spring5webapp.repositories;
+
+import melp.springframework.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/melp/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package melp.springframework.spring5webapp.repositories;
+
+import melp.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
